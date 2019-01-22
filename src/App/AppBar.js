43674ed486166082d6f1adf7930a08f07ab5a846
marb61a <marb61a@gmail.com>
2019-01-22 17:11:58 +0000

    updated appbar

--- src/App/AppBar.js
+++ src/App/AppBar.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Bar = styled.div `
-    display: grid
+const Logo = styled.div `
+    font-size: 1.5em;
 `;
 
+const Bar = styled.div `
+    display: grid;
+    grid-template-columns: 180px auto 100px 100px;
+`;
+
+const ControlButtonElem = styled.div `
+    cursor: pointer;
+    ${props => props.active && css `
+    
+    `}
+`; 
+
+function ControlButton({name}) {
+
+}
+i
 export default function () {
-    <div>Hey</div>
+    return (
+        <Bar>
+            <Logo>ReactCrypto</Logo>
+            <div>Dashboard</div>
+            <div>Settings</div>
+        </Bar>
+    )
 }
