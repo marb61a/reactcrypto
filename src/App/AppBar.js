c04b7fe67a11c4c1c558737b030eb8ba3f07f54f
marb61a <marb61a@gmail.com>
2019-01-22 16:54:53 +0000

    added initial appbar

---
+++ src/App/AppBar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Bar = styled.div `
+    display: grid
+`;
+
+export default function () {
+    <div>Hey</div>
+}
--- src/App/index.js
+++ src/App/index.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import AppLayout from './AppLayout';
+import AppBar from './AppBar';
+
 class App extends Component {
   render() {
     return (
-     <div>Hello World</div>
+     <AppLayout>
+       <AppBar />
+     </AppLayout>
     );
   }
 }
