609dab1cfd2489b13dd152aad9a2718b11e11514
marb61a <marb61a@gmail.com>
2019-01-29 16:08:32 +0000

    added shared tile file

---
+++ src/Shared/Tile.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+import {subtleBoxShadow, lightBlueBackground, greenBoxShadow, redBoxShadow} from "./Styles";
+
+export const Tile = styled.div`
+    ${subtleBoxShadow}
+    ${lightBlueBackground}
+    padding: 10px;   
+`;
