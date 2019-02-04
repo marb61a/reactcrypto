e82141ce2398e3a1b2649c911c7b336ad743c760
marb61a <marb61a@gmail.com>
2019-02-04 16:16:36 +0000

    installed fuzzy and updated search

--- src/Settings/Search.js
+++ src/Settings/Search.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import _ from 'lodash';
+import fuzzy from 'fuzzy';
 
 import {backgroundColor2, fontSize2} from "../Shared/Styles";
 import {AppContext} from "../App/AppProvider";
@@ -19,7 +20,7 @@ const SearchInput = styled.input`
     place-self: center left; 
 `;
 
-const handleFilter = _.debounce((inputValue, coinList, setFilteredCoins) => {
+const handleFilter = _.debounce((inputValue, coinList, setFilterCoins) => {
     // Get all the coin symbols
     let coinSymbols = Object.keys(coinList);
 
@@ -27,6 +28,15 @@ const handleFilter = _.debounce((inputValue, coinList, setFilteredCoins) => {
     let coinNames = coinSymbols.map(sym => coinList[sym].CoinName);
     let allStringsToSearch = coinSymbols.concat(coinNames);
 
+    let fuzzyResults = fuzzy 
+        .filter(inputValue, allStringsToSearch, {})
+        .map(result => result.string);
+
+    let filteredCoins = _.pickBy(coinList, (result, symkey) => {
+        return (_.includes(fuzzyResults, symKey) || _.includes(fuzzyResults, coinName));
+    });
+
+    setFilterCoins(filteredCoins);
 }, 500);
 
 function filterCoins(e, setFilteredCoins, coinList){
