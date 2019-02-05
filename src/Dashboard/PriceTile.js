38d2f0dacc20d760c7e798ab41311e257ccbd7c7
marb61a <marb61a@gmail.com>
2019-02-05 15:58:01 +0000

    adding functions to PriceTile

--- src/Dashboard/PriceTile.js
+++ src/Dashboard/PriceTile.js
@@ -13,6 +13,41 @@ const JustifyLeft = styled.div`
   justify-self: left; 
 `;
 
+const TickerPrice = styled.div`
+  ${fontSizeBig};
+`;
+
+const ChangePct = styled.div`
+  color: green; 
+  ${props => props.red && css`
+    color: red; 
+  `}
+`;
+
+const numberFormat = number => {
+  return +(number + '').slice(0, 7);
+};
+
+const PriceTileStyled = styled(SelectableTile)`
+
+`;
+
+function ChangePercent({data}) {
+  <JustifyRight>
+    <ChangePct red={data.CHANGEPCT24HOUR < 0}>
+      {numberFormat(data.CHANGEPCT24HOUR)}%
+    </ChangePct>
+  </JustifyRight>
+}
+
+function PriceTile({sym, data}) {
+  return (
+    <PriceTileStyled>
+      
+    </PriceTileStyled>
+  )
+}
+
 export default function({price, index}) {
     return(
         <AppContext.Consumer>
