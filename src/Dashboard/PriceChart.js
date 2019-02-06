9212b621152e1f1b060563f45f9e3b32ce37d0af
marb61a <marb61a@gmail.com>
2019-02-06 16:41:14 +0000

    added ChartSelect and updated PriceChart

--- src/Dashboard/PriceChart.js
+++ src/Dashboard/PriceChart.js
@@ -1,16 +1,20 @@
 import highchartsConfig from './HighchartsConfig';
 import React from 'react';
+import ReactHighcharts from 'react-highcharts';
+
 import {Tile} from "../Shared/Tile";
 import {AppContext} from "../App/AppProvider";
-import ReactHighcharts from 'react-highcharts';
+import ChartSelect from './ChartSelect';
 
 export default function() {
     return (
         <AppContext.Consumer>
             {
-                () => 
+                ({changeChartSelect}) => 
                 <Tile>
-                    
+                    <ChartSelect>
+                        
+                    </ChartSelect>
                 </Tile>
             }
         </AppContext.Consumer>
